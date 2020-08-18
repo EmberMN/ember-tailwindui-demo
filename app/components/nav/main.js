14b67445d6dffa390d82fb98f4f0421494d093d1
Jacob Quant <jacobq@gmail.com>
2020-08-18 18:37:22 -0500

    fix: populate main nav from parameters

--- app/components/nav/main.js
+++ app/components/nav/main.js
@@ -1,9 +1,11 @@
 import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class NavMainComponent extends Component {
   @tracked isMobileNavOpen = false;
+  @service router;
 
   @action
   toggleMobileNav() {
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,25 +1,12 @@
-import { tracked } from '@glimmer/tracking';
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
-// TODO: Not sure if it really makes sense to mark these as tracked because:
-//       1. We'll probably keep it constant
-//       2. I don't think @tracked deeply inspects arrays or objects
-//       3. ...profit?
-class NavigationData {
-  @tracked mainItems = [];
-  @tracked userMenuItems = [];
-}
-
 export default class ApplicationController extends Controller {
   @service router;
-  @service localData;
+  @service localData; // TODO: We don't actually use this (yet)
 
-  navigation = new NavigationData();
-  constructor() {
-    super(...arguments);
-
-    this.navigation.mainItems = [{
+  navigation = {
+    mainItems: [{
       text: 'Home',
       route: 'index'
     }, {
@@ -28,14 +15,13 @@ export default class ApplicationController extends Controller {
     }, {
       text: 'Table',
       route: 'table'
-    }];
-
-    this.navigation.userMenuItems = [{
+    }],
+    userMenuItems: [{
       text: 'Settings',
       route: 'settings'
     }, {
       text: 'Sign out',
       route: 'sign-out'
-    }];
-  }
+    }]
+  };
 }
