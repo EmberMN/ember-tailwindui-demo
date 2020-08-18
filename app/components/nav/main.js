52fdd917c5308f3bcb30a7f5312a74e1e9c8366a
Jacob Quant <jacobq@gmail.com>
2020-08-18 13:29:18 -0500

    feat: make mobile menu open/close functionality work

--- app/components/nav/main.js
+++ app/components/nav/main.js
@@ -1,4 +1,12 @@
+import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class NavMainComponent extends Component {
+  @tracked isMobileNavOpen = false;
+
+  @action
+  toggleMobileNav() {
+    this.isMobileNavOpen = !this.isMobileNavOpen;
+  }
 }
