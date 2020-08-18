e2ceb7d0d118ff09e0256bf6947fcf682ceb4b98
Jacob Quant <jacobq@gmail.com>
2020-08-18 18:58:45 -0500

    WIP: time to start the meet-up; doh!

--- app/components/nav/mobile.js
+++ app/components/nav/mobile.js
@@ -1,4 +1,6 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class NavMobileComponent extends Component {
+  @service router;
 }
